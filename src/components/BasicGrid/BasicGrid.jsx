14b345c69bb47faecd19e462aae8d1e0013b31d2
CtrlAltCode7 <mahendray@precisio.tech>
2024-04-17 19:19:11 +0530

    Fixed the issue of Selection options.

--- src/components/BasicGrid/BasicGrid.jsx
+++ src/components/BasicGrid/BasicGrid.jsx
@@ -5,7 +5,7 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import FooterAccordionForMobile from "../../pages/Footer/FooterAccordionForMobile";
 import AccordionExpandIcon from "../Accordion/AccordionExpandIcon";
-import { Typography } from "@mui/material";
+import { Button, Pagination, Switch, Typography } from "@mui/material";
 import SelectComponent from "../SelectComponent/SelectComponent";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -18,15 +18,16 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function BasicGrid() {
+  const label = { inputProps: { "aria-label": "Size switch demo" } };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={4}>
+        <Grid xs={12} sm={6} md={4} lg={3}>
           <Item>
             <AccordionExpandIcon />
           </Item>
         </Grid>
-        <Grid item xs={8}>
+        <Grid xs={12} sm={6} md={8} lg={9}>
           <Item>
             <Typography
               variant="h4"
@@ -49,20 +50,181 @@ export default function BasicGrid() {
           </Item>
           <Item
             sx={{
-              paddingTop: "1rem",
+              paddingTop: "0 1rem",
               overflow: "hidden !important",
               display: "flex",
-              flexGrow: 1
+              justifyContent: "space-evenly !important",
+              // flexGrow: 1,
+              padding: "1rem 1rem 0",
+              "@media (max-width: 600px)": {
+                flexDirection: "column",
+              },
             }}
           >
+            <Box
+              sx={{
+                backgroundColor: "#eee",
+                flexDirection: "row",
+                display: "flex",
+                gap: "1rem",
+                padding: "1rem",
+                // margin: "0 1rem",
+                paddingTop: "2rem",
+                flexWrap: "wrap",
 
-              <SelectComponent />
+                "@media (max-width: 600px)": {
+                  flexDirection: "column",
+                  margin: 0,
+                },
+              }}
+            >
+              <SelectComponent title="Finger Size" minWidthSize="200" />
+              <SelectComponent title="Primary Stone Count" minWidthSize="200" />
+              <SelectComponent title="Primary Stone Shape" minWidthSize="200" />
+              <SelectComponent title="Metal Color" minWidthSize="200" />
+            </Box>
+          </Item>
+          <Item
+            sx={{
+              padding: "0 1.4rem ",
+              justifyContent: "space-evenly !important",
+              "@media (max-width: 600px)": {
+                padding: "0 1rem ",
+              },
+            }}
+          >
+            <Box
+              sx={{
+                background: "#eee",
+                textAlignLast: "end",
+              }}
+            >
+              <Button
+                variant="contained"
+                sx={{
+                  backgroundColor: "#fff", // Set background color
+                  color: "#555", // Set text color
+                  border: "1px solid #999",
+                  fontSize: "12px",
+                  margin: ".5rem 1rem 1rem",
+                  "&:hover": {
+                    backgroundColor: "#999", // Change background color on hover
+                    color: "#ffffff", // Change text color on hover
+                  },
+                }}
+              >
+                Reset Selections
+              </Button>
+            </Box>
+          </Item>
+          <Item
+            sx={{
+              padding: "0 1.4rem ",
+            }}
+          >
+            <Box
+              component={"p"}
+              sx={{
+                padding: "1.5rem",
+                backgroundColor: "#eee",
+              }}
+            >
+              <Typography
+                variant="body2"
+                sx={{
+                  display: "flex !important",
+                  justifyContent: "center",
+                  wordWrap: "break-word",
+                  fontWeight: "bold",
+                }}
+              >
+                <Switch {...label} defaultChecked size="small" />
+                Ready to Ship
+                <Typography variant="body2">
+                  - Only show products that have at least one in-stock option
+                </Typography>
+              </Typography>
+            </Box>
+          </Item>
+          <Item
+            sx={{
+              margin: "0 1rem",
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <Typography variant="body2" mr={1}>
+                  Sort by
+                </Typography>
+                <SelectComponent />
+              </Box>
+              <Box
+                sx={{
+                  display: { xs: "none", md: "flex", lg: "flex" },
+                  gap: "1rem",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>Showing 1 - 36 of 1226</Typography>|
+                <Typography>Items per page</Typography>
+                <SelectComponent />
+              </Box>
+              <Box
+                sx={{
+                  display: { xs: "flex", md: "none", lg: "none" },
+                  alignItems: "center",
+                }}
+              >
+                <Typography
+                  variant="body2"
+                  sx={{
+                    display: { xs: "flex", md: "none", lg: "none" },
+                  }}
+                >
+                  Items:
+                </Typography>
 
-              <SelectComponent />
-              <SelectComponent />
+                <SelectComponent />
+              </Box>
+            </Box>
+          </Item>
+          <Item
+            sx={{
+              margin: "0 1rem",
+              display: "flex",
+              justifyContent: "end",
+            }}
+          >
+            <Pagination
+              count={10}
+              variant="outlined"
+              shape="rounded"
+              color="primary"
+              sx={{
+                '& .Mui-selected': {
+                  backgroundColor: 'blue', // Customize background color of selected button
+                  color: 'white', // Customize text color of selected button
+                },
+                '& button:not(.Mui-selected)': {
+                  backgroundColor: 'lightgray', // Customize background color of other buttons
+                  color: 'black', // Customize text color of other buttons
+        
+                },
 
-              <SelectComponent />
-
+              }}
+              
+            />
           </Item>
         </Grid>
       </Grid>
--- src/components/SelectComponent/SelectComponent.jsx
+++ src/components/SelectComponent/SelectComponent.jsx
@@ -5,8 +5,10 @@ import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
 
-export default function SelectComponent() {
+export default function SelectComponent({title,minWidthSize}) {
+  console.log('minWidthSize', minWidthSize);
   const [age, setAge] = React.useState("");
 
   const handleChange = (event) => {
@@ -14,16 +16,73 @@ export default function SelectComponent() {
   };
 
   return (
-    <Grid container justifyContent="center">
-      <Grid item xs={12} sm={6} md={4}>
-        <FormControl fullWidth sx={{ m: 1 }}>
+    <Grid
+      sx={{
+        background: "#eee",
+        borderRadius: "5px",
+        // padding: "1rem 0"
+      }}
+    >
+      <Grid
+        sx={
+          {
+            // padding: 2
+          }
+        }
+      >
+        <FormControl
+          fullWidth
+          sx={{
+            // m: 1,
+            minWidth: parseInt(minWidthSize),
+            minHeight: 35,
+            gap: "1rem",
+            "@media (max-width: 1200px) and (min-width: 600px)": {
+              /* This rule will apply when the screen width is between 300px and 600px */
+              width: "260px !important",
+            },
+          }}
+        >
+          {title && (
+            <Typography
+              sx={{
+                alignSelf: "baseline",
+                fontSize: "18px",
+                fontWeight: "bold",
+                color: "#555",
+              }}
+            >
+              {title}
+            </Typography>
+          )}
+
           <Select
             value={age}
             onChange={handleChange}
             displayEmpty
+            disableScrollLock={true}
+            MenuProps={{ disableScrollLock: true }}
+            slotProps={{
+              root: {
+                sx: {
+                  backgroundColor: "#fff",
+                  "& .MuiSelect-outlined": {
+                    display: "flex",
+                    padding: "10px 14px !important",
+                  },
+                  ".MuiNativeSelect-icon": {
+                    color: "red",
+                  },
+                },
+              },
+            }}
+
+            // sx={{
+            //   padding: "10px 14px !important"
+            // }}
           >
             <MenuItem value="">
-              <em>Select</em>
+              <span>Select</span>
             </MenuItem>
             <MenuItem value={10}>Ten</MenuItem>
             <MenuItem value={20}>Twenty</MenuItem>
