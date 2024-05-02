65ecdd63559b21e109b608ab46c4b498a732c68a
CtrlAltCode7 <mahendray@precisio.tech>
2024-05-03 02:00:48 +0530

    fixed the login elevation

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,7 +8,7 @@ import CreateAccountSection from "../../components/createAccountTextandButton/cr
 export default function Login() {
   return (
     <Layout>
-      <Grid
+      {/* <Grid
         container
         spacing={0}
         justifyContent="space-between"
@@ -40,7 +40,7 @@ export default function Login() {
               </Typography>
             </Box>
         </Grid>
-      </Grid>
+      </Grid> */}
       {/* <div
         style={{
           background: "red",
@@ -116,11 +116,23 @@ export default function Login() {
           Hello
         </div>
       </div> */}
-      {/* <div className="parent">
-        <div className="child">hi</div>
-        <div className="child">hello</div>
-      </div> */}
-
+      <div className="parent">
+        <div className="child">
+          <LoginForm />
+        </div>
+        <div className="child">
+          <CreateAccountSection
+            title="Create Account"
+            description="Sign up for a new Stuller account."
+            buttonText="Create Account"
+          />
+          <CreateAccountSection
+            title="Create Account"
+            description="Sign up for a new Stuller account."
+            buttonText="Create Account"
+          />
+        </div>
+      </div>
     </Layout>
   );
 }
