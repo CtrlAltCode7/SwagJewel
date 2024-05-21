0734c3912f5212186bee36d42d1a6523ea86264a
CtrlAltCode7 <mahendray@precisio.tech>
2024-05-21 14:42:32 +0530

    Added catalogs sliders and its cprrespoding data.

--- src/pages/catalogs/catalogSlider/catalogSlider.jsx
+++ src/pages/catalogs/catalogSlider/catalogSlider.jsx
@@ -5,11 +5,13 @@ import "slick-carousel/slick/slick-theme.css";
 import BookCard from "../../../components/carousel/book";
 import { useEffect } from "react";
 import { useState } from "react";
+import "./catalogSlider.css";
+import { Box } from "@mui/material";
 
 export const baseUrl =
   "https://meteor.stullercloud.com/das/128648010?obj=stones/diamonds/g_Accent1&obj=stones/diamonds/g_Accent2&obj=stones/diamonds/g_Accent3&obj=metals&obj=metals&obj.recipe=rose&$xlarge$";
 
-function PreviousNextMethods() {
+function PreviousNextMethods({ displayData }) {
   const [imageUrlsWithGroupDescription, setImageUrlsWithGroupDescription] =
     useState([]);
   let sliderRef = useRef(null);
@@ -25,6 +27,33 @@ function PreviousNextMethods() {
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 5,
+    className: "catalogs-settings",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   const imageUrls = [
@@ -36,13 +65,13 @@ function PreviousNextMethods() {
 
   const fetchBooks = async () => {
     try {
-      const response = await fetch(`/productsListing.json`)
+      const response = await fetch(`/${displayData}.json`)
         .then((res) => res.json())
         .then((data) => {
           return data;
         });
 
-      // console.log("API response:", response);
+      console.log("API response:", response);
       setImageUrlsWithGroupDescription(response);
     } catch (err) {
       console.error(err.message);
@@ -53,7 +82,7 @@ function PreviousNextMethods() {
   useEffect(() => {
     fetchBooks();
     // eslint-disable-next-line
-  }, []);
+  }, [displayData]);
   return (
     <div className="slider-container">
       <Slider
@@ -71,11 +100,11 @@ function PreviousNextMethods() {
             className="slide-content"
             style={{
               textAlign: "center !important",
-              // margin: "10px 38px "
+              // margin: "10px 38px"
             }}
           >
             <img
-              src={product.urls[0]}
+              src={product.img_src}
               alt={product.title}
               style={{
                 margin: "auto",
@@ -85,35 +114,64 @@ function PreviousNextMethods() {
               className="product-description"
               style={{
                 textAlign: "center",
-                fontWeight: "bold"
+                fontWeight: "bold",
               }}
             >
-              {product.groupDescription}
+              {product.text}
             </p>
             <p
               className="product-description1"
               style={{
                 textAlign: "center",
-                marginTop: 0
+                marginTop: 0,
               }}
             >
-              {product.publish}
+              {product.year}
             </p>
           </div>
         ))}
       </Slider>
-      <div style={{ textAlign: "center", marginTop: "2rem" }}>
-        <button className="button" onClick={previous}>
-          Previous
-        </button>
-        <button className="button" onClick={next}>
+      <div style={{ textAlign: "center", margin: "2.5rem 1rem .5rem 1.5rem" }}>
+        <span className="catalog-button prev-btn" onClick={previous}>
+          Prev
+        </span>
+        <span className="catalog-button nxt-btn" onClick={next}>
           Next
-        </button>
+        </span>
       </div>
+      <div
+        style={{
+          borderBottom: "1px solid",
+        }}
+      ></div>
     </div>
   );
 }
 
-export default function CatalogSlider() {
-  return PreviousNextMethods();
+export default function CatalogSlider({ title }) {
+  const camelCaseTitle = title?.replace(/\s+(.)/g, function(match, group1) {
+    return group1.toUpperCase();
+  });const lowercaseTitle = camelCaseTitle.charAt(0).toLowerCase() + camelCaseTitle.slice(1);
+  console.log('lowercaseTitle', lowercaseTitle)
+  return (
+    <>
+      <Box
+        sx={{
+          fontSize: {
+            xs: 20,
+            sm: 22,
+            md: 24,
+            lg: 26,
+            xl: 27,
+          },
+          fontWeight: 100,
+          color: "#555",
+          marginTop: ".5rem",
+        }}
+      >
+        {title}
+      </Box>
+      <PreviousNextMethods displayData={lowercaseTitle} />
+    </>
+  );
 }
--- src/pages/catalogs/index.jsx
+++ src/pages/catalogs/index.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Layout from "../../components/layout";
 import CatalogsHeader from "./catalogsHeader/catalogsHeader";
-import "./index.css"
+import "./index.css";
 import CatalogSlider from "./catalogSlider/catalogSlider";
+import { Box } from "@mui/material";
 
 export default function Catalogs() {
+  const sectionTitles = [
+    "New Arrivals",
+    "Catalogs",
+    "Brochures",
+    "Product Guides",
+    "From the Bench Publications",
+    "Beyond the Glass Publication",
+  ];
   return (
     <Layout>
       <div className="catalogsContainer">
         <CatalogsHeader />
-        <CatalogSlider />
+        {sectionTitles.map((title, index) => (
+          <CatalogSlider key={index} title={title} />
+        ))}
       </div>
     </Layout>
   );
