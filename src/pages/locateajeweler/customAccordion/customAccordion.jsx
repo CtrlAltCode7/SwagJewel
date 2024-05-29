a62cc9713ddbb0a0d78a76c6efdd6f28810f5bc0
CtrlAltCode7 <mahendray@precisio.tech>
2024-05-29 11:50:16 +0530

    Added chnages

--- src/pages/locateajeweler/customAccordion/customAccordion.jsx
+++ src/pages/locateajeweler/customAccordion/customAccordion.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 
 const CustomAccordion = ({ title, details }) => {
   return (
@@ -35,19 +35,29 @@ const CustomAccordion = ({ title, details }) => {
         <Typography variant="subtitle1">{title}</Typography>
       </AccordionSummary>
       <AccordionDetails>
-        {typeof details === 'string' ? (
-          <Typography variant="body2">{details}</Typography>
-        ) : (
-          <Table>
-            <TableBody>
-              {details.map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell>{row.day}</TableCell>
-                  <TableCell>{row.hours}</TableCell>
-                </TableRow>
+        {Array.isArray(details) ? (
+          details.length > 0 && typeof details[0] === "object" ? (
+            <Table>
+              <TableBody>
+                {details.map((row, index) => (
+                  <TableRow key={index}>
+                    <TableCell>{row.day}</TableCell>
+                    <TableCell>{row.hours}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <ul>
+              {details.map((item, index) => (
+                <li key={index}>
+                  <Typography variant="body2">{item}</Typography>
+                </li>
               ))}
-            </TableBody>
-          </Table>
+            </ul>
+          )
+        ) : (
+          <Typography variant="body2">{details}</Typography>
         )}
       </AccordionDetails>
     </Accordion>
